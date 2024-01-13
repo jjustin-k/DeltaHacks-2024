3b1944baf5b0f87d69897e244b41f264f89ab82c
jjustin-k <jjkwinecki@gmail.com>
2024-01-13 12:55:46 -0500

    Start

--- Main.py
+++ Main.py
@@ -5,26 +5,35 @@ import numpy as np
 import os
 import math
 
-
+cap = cv2.VideoCapture(0)
 detector = HandDetector(maxHands=2, detectionCon=0.8)
+model = cap
+BaseOptions = mp.tasks.BaseOptions
+GestureRecognizer = mp.tasks.vision.GestureRecognizer
+GestureRecognizerOptions = mp.tasks.vision.GestureRecognizerOptions
+VisionRunningMode = mp.tasks.vision.RunningMode
 
 
-def display():
-    cap = cv2.VideoCapture(0)
-    process_frame = True
-    while True:
-        ret, raw_frame = cap.read()
-        focus_frame = np.ascontiguousarray(raw_frame)
+process_frame = True
+
+while True:
+    ret, raw_frame = cap.read()
+    focus_frame = np.ascontiguousarray(raw_frame)
+
+    mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=focus_frame)
+
+    options = GestureRecognizerOptions(
+        base_options=BaseOptions(model_asset_path=model),
+        running_mode=VisionRunningMode.VIDEO)
+    with GestureRecognizer.create_from_options(options) as recognizer:
+        gesture_recognition = recognizer.recognize(mp_image)
         cv2.imshow('Vid', raw_frame)
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
-    cap.release()
-    cv2.destroyAllWindows()
-    return
+cap.release()
+cv2.destroyAllWindows()
 
-
-display()
 
 
 
