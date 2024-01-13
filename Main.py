d84367f304770addb811795737e9fdabf3bc3d83
jjustin-k <jjkwinecki@gmail.com>
2024-01-13 12:00:21 -0500

    Start

--- Main.py
+++ Main.py
@@ -1,4 +1,16 @@
 import cv2
 from cvzone.HandTrackingModule import HandDetector
+import numpy as np
+import os
 
-print("Hello World")
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cap.read()
+    cv2.imshow('video', frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+cap.release()
+cv2.destroyAllWindows()
+
