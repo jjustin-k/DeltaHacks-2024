6bcd2691a5b4cc1a375e1fc4fb4a29d8d69dd303
jjustin-k <jjkwinecki@gmail.com>
2024-01-13 12:27:39 -0500

    Start

--- Main.py
+++ Main.py
@@ -1,16 +1,30 @@
 import cv2
 from cvzone.HandTrackingModule import HandDetector
+import mediapipe as mp
 import numpy as np
 import os
+import math
 
-cap = cv2.VideoCapture(0)
 
-while True:
-    ret, frame = cap.read()
-    cv2.imshow('video', frame)
+detector = HandDetector(maxHands=2, detectionCon=0.8)
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-cap.release()
-cv2.destroyAllWindows()
+
+def display():
+    cap = cv2.VideoCapture(0)
+    process_frame = True
+    while True:
+        ret, raw_frame = cap.read()
+        focus_frame = np.ascontiguousarray(raw_frame)
+        cv2.imshow('Vid', raw_frame)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    cap.release()
+    cv2.destroyAllWindows()
+    return
+
+
+display()
+
+
 
