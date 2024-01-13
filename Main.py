98d14eea916b4d743f597e9d3c3c17f22e27823a
jjustin-k <jjkwinecki@gmail.com>
2024-01-13 17:00:26 -0500

    Start

--- Main.py
+++ Main.py
@@ -1,5 +1,8 @@
 import cv2
 from cvzone.HandTrackingModule import HandDetector
+from mediapipe.framework.formats import landmark_pb2
+from mediapipe.python import solutions
+from mediapipe.tasks.python import vision
 import HandLocation
 import mediapipe as mp
 import numpy as np
@@ -11,21 +14,47 @@ cap = cv2.VideoCapture(0)
 
 process_frame = True
 
+
+def img_landmarks(rgb_image, detection_results: mp.tasks.vision.HandLandmarkerResult):
+    try:
+        if detection_results.hand_landmarks == []:
+            return rgb_image
+        else:
+            hand_landmarks_list = detection_results.hand_landmarks
+            annotated_image = np.copy(rgb_image)
+
+            for i in range(len(hand_landmarks_list)):
+                hand_landmarks = hand_landmarks_list[i]
+
+                hand_landmarks_exp = landmark_pb2.NormalizedLandmarkList()
+                hand_landmarks_exp.landmark.extend([
+                    landmark_pb2.NormalizedLandmark(x=landmark.x, y=landmark.y, z=landmark.z)
+                    for landmark in hand_landmarks])
+                mp.solutions.drawing_utils.draw_landmarks(
+                    annotated_image,
+                    hand_landmarks_exp,
+                    mp.solutions.hands.HAND_CONNECTIONS,
+                    mp.solutions.drawing_styles.get_default_hand_landmarks_style(),
+                    mp.solutions.drawing_styles.get_default_hand_connections_style())
+            return annotated_image
+    except:
+        return rgb_image
+
+
 while True:
-    ret, raw_frame = cap.read()
-    focus_frame = np.ascontiguousarray(raw_frame)
+    ret, frame = cap.read()
 
     model = HandLocation.HandLocation()
-    model.detect_async(raw_frame)
+    model.create_landmark()
+    model.detect_async(frame)
+
+    frame = img_landmarks(frame, model.result)
 
     print(model.result)
-    cv2.imshow('Vid', raw_frame)
+    cv2.imshow('Vid', frame)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
+model.close()
 cap.release()
 cv2.destroyAllWindows()
-
-
-
-
